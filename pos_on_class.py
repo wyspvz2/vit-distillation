50181b860a32298d6d7972fe0594a64636a37ac1
wyspvz2 <68979551+wyspvz2@users.noreply.github.com>
2025-04-13 17:29:10 +0800

    Update pos_on_class.py

--- pos_on_class.py
+++ pos_on_class.py
@@ -10,7 +10,7 @@ def project_teacher_mean_to_student(pseudo_teacher_features, teacher_mean_featur
         teacher_mean_feature: 教师模型的类别均值特征，形状为 [197, 1024]
         device: 运行设备
     Returns:
-        projected_features: 投影后的特征，形状为 [B, 197, 1024]
+        projected_features_S: 投影后的特征，形状为 [B, 197, 1024]
     """
     pseudo_teacher_features = pseudo_teacher_features.to(device)  # [B, 197, 1024]
     teacher_mean_feature = teacher_mean_feature.to(device)  # [197, 1024]
@@ -23,9 +23,9 @@ def project_teacher_mean_to_student(pseudo_teacher_features, teacher_mean_featur
     teacher_mean_expanded_normalized = F.normalize(teacher_mean_expanded, p=2, dim=-1)  # [B, 197, 1024]
 
     # 计算学生特征到教师均值特征的投影，投影使用点乘
-    projected_features = torch.bmm(pseudo_teacher_features_normalized, teacher_mean_expanded_normalized.transpose(1, 2))  # [B, 197, 197]
+    projected_features_S = torch.bmm(pseudo_teacher_features_normalized, teacher_mean_expanded_normalized.transpose(1, 2))  # [B, 197, 197]
 
-    return projected_features
+    return projected_features_S
 
 @torch.no_grad()
 def project_teacher_features_to_class_mean(teacher_features, teacher_mean_feature, device='cuda'):
@@ -36,7 +36,7 @@ def project_teacher_features_to_class_mean(teacher_features, teacher_mean_featur
         teacher_mean_feature: 教师模型的类别均值特征，形状为 [197, 1024]
         device: 运行设备
     Returns:
-        projected_features: 投影后的特征，形状为 [B, 197, 1024]
+        projected_features_T: 投影后的特征，形状为 [B, 197, 1024]
     """
     teacher_features = teacher_features.to(device)  # [B, 197, 1024]
     teacher_mean_feature = teacher_mean_feature.to(device)  # [197, 1024]
@@ -49,6 +49,6 @@ def project_teacher_features_to_class_mean(teacher_features, teacher_mean_featur
     teacher_mean_expanded_normalized = F.normalize(teacher_mean_expanded, p=2, dim=-1)  # [B, 197, 1024]
 
     # 计算教师特征到教师均值特征的投影，投影使用点乘
-    projected_features = torch.bmm(teacher_features_normalized, teacher_mean_expanded_normalized.transpose(1, 2))  # [B, 197, 197]
+    projected_features_T = torch.bmm(teacher_features_normalized, teacher_mean_expanded_normalized.transpose(1, 2))  # [B, 197, 197]
 
-    return projected_features
+    return projected_features_T
