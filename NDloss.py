ba09f4086ddf7c8e5d99a8db38096320273c7072
wyspvz2 <68979551+wyspvz2@users.noreply.github.com>
2025-04-13 17:57:43 +0800

    Update NDloss.py

--- NDloss.py
+++ NDloss.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn.functional as F
 import pos_on_class
-def compute_projection_l2_loss(student_features, teacher_features, labels, num_classes, device='cuda'):
+def compute_projection_l2_loss(student_features, teacher_features, projected_features_S, projected_features_T, labels, num_classes, device='cuda'):
     """
     计算教师和伪教师投影后的L2损失，并对每个类别求平均，再对所有类别的损失求均值
     Args:
         student_features: 伪教师特征，形状为 [B, 197, 1024]
-        teacher_features: 教师特征，形状为 [B, 197, 1024]
+        projected_features_T: 教师特征，形状为 [B, 197, 1024]
         labels: 每个样本的标签，形状为 [B]
         num_classes: 类别数 C
         device: 运行设备
@@ -15,8 +15,9 @@ def compute_projection_l2_loss(student_features, teacher_features, labels, num_c
     """
     student_features = student_features.to(device)  # [B, 197, 1024]
     teacher_features = teacher_features.to(device)  # [B, 197, 1024]
+    projected_features_S= projected_features_S.to(device)  # [B, 197, 1024]
+    projected_features_T= projected_features_T.to(device)  # [B, 197, 1024]
     labels = labels.to(device)  # [B]
-
     total_loss = 0.0
     for k in range(num_classes):
         # 获取当前类别 k 的样本索引
@@ -27,10 +28,10 @@ def compute_projection_l2_loss(student_features, teacher_features, labels, num_c
         
         # 对于属于当前类别 k 的所有样本
         class_student_features = student_features[class_indices]  # 形状 [N_k, 197, 1024]
-        class_teacher_features = teacher_features[class_indices]  # 形状 [N_k, 197, 1024]
+        class_projected_features_T = projected_features_T[class_indices]  # 形状 [N_k, 197, 1024]
 
         # 计算L2损失
-        l2_loss = torch.norm(class_student_features - class_teacher_features, p=2, dim=-1)  # [N_k, 197]
+        l2_loss = torch.norm(class_student_features - class_projected_features_T, p=2, dim=-1)  # [N_k, 197]
         
         # 计算教师和伪教师特征的L2范数
         student_norm = torch.norm(class_student_features, p=2, dim=-1)  # [N_k, 197]
